1a4b73ec68230552be72abcc426422a138edd146
blusno <blusno@126.com>
2016-05-16 15:06:11 +0800

    修改代码

--- OrderedListSort.cpp
+++ OrderedListSort.cpp
@@ -8,22 +8,14 @@
 #define LENGTH(s) (sizeof(s) / sizeof(int))
 
 
-bool sort( int a, int const b[],int* index){
-    int j=(*index);
-    for(;j<LENGTH(b);j++){
-        puts("index:");
-        printf("%d\n",j);
-        puts("第一个数组中的值为：");
-        printf("%d\n",a);
-        puts("第二个数组中的值为：");
-        printf("%d\n",b[j]);
+bool sort( int a, int const b[],int bSize,int* index){
+    for(int j=(*index);j<bSize;j++){
+        *index = j;
         if(a==b[j]){
-            j++;
-            *index = j;
+            *index++;
             return true;
         }
     }
-    *index = j;
     return false;
 }
 
@@ -33,16 +25,14 @@ int main(){
     int b[6]={2,3,4,5,5,7};
     int index = 0;
     int cIndex =0;
+    puts("两个数组中相等的元素为：");
     for (int i = 0; i < LENGTH(a); ++i) {
-        int t = a[i];
-        if (sort(t,b,&index)){
-            c[cIndex]=t;
+        if (sort(a[i],b,LENGTH(b),&index)){
+            c[cIndex]=a[i];
+            printf("cIndex: %d\n",cIndex);
+            printf("%d\n",c[cIndex]);
             cIndex++;
         }
-    }
-    for (int j = 0; j <LENGTH(c) ; ++j) {
-        puts("两个数组中相等的元素为：");
-        printf("%d\n",c[j]);
     }
     return 0;
 }
